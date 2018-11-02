c3690c4c691d8fab86e1fe42b194353281c4c2dc
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2018-11-02 14:19:42 +0100

    file handling

--- src/PSI4.cpp
+++ src/PSI4.cpp
@@ -99,21 +99,21 @@ void PSI4Charges(vector<QMMMAtom>& QMMMData, QMMMSettings& QMMMOpts, int bead)
   call << "mkdir LICHM_PSI_LASTITER";
   globalSys = system(call.str().c_str());
   //
-  //call.str("");
+  call.str("");
   call << "mv ";
   call << "LICHM_" << bead << ".dat ";
   call << "LICHM_PSI_LASTITER/.";
-  //globalSys = system(call.str().c_str());
+  globalSys = system(call.str().c_str());
   call << " ";
   //
-  //call.str("");
+  call.str("");
   call << "mv ";
   call << "LICHM_" << bead << ".out ";
   call << "LICHM_PSI_LASTITER/.";
-  //globalSys = system(call.str().c_str());
+  globalSys = system(call.str().c_str());
   call << " ";
   //
-  //call.str("");
+  call.str("");
   call << "mv ";
   call << "LICHM_" << bead << ".log ";
   call << "LICHM_PSI_LASTITER/.";
@@ -256,16 +256,16 @@ double PSI4Energy(vector<QMMMAtom>& QMMMData, QMMMSettings& QMMMOpts, int bead)
   call << "LICHM_" << bead << ".dat ";
   call << "LICHM_PSI_LASTITER/.";
   call << " ";
-  //globalSys = system(call.str().c_str());
+  globalSys = system(call.str().c_str());
   //
-  //call.str("");
+  call.str("");
   call << "mv ";
   call << "LICHM_" << bead << ".out ";
   call << "LICHM_PSI_LASTITER/.";
-  //globalSys = system(call.str().c_str());
+  globalSys = system(call.str().c_str());
   call << " ";
   //
-  //call.str("");
+  call.str("");
   call << "mv ";
   call << "LICHM_" << bead << ".log ";
   call << "LICHM_PSI_LASTITER/.";
