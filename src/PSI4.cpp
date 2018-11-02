4345bd3cbbd3eef5fc593eb4937db43464564777
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2018-11-02 14:15:51 +0100

    fix second type

--- src/PSI4.cpp
+++ src/PSI4.cpp
@@ -267,7 +267,7 @@ double PSI4Energy(vector<QMMMAtom>& QMMMData, QMMMSettings& QMMMOpts, int bead)
   //
   //call.str("");
   call << "mv ";
-  call << "LICHM_" << bead << ".log";
+  call << "LICHM_" << bead << ".log ";
   call << "LICHM_PSI_LASTITER/.";
   globalSys = system(call.str().c_str());
   //Change units
